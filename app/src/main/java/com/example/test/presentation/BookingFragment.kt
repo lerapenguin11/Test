4d8cec0bebc63e7a93cef84fe7b9fa1650dcc908
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-11-21 18:52:05 +0500

    add mask phone and validate email

--- app/src/main/java/com/example/test/presentation/BookingFragment.kt
+++ app/src/main/java/com/example/test/presentation/BookingFragment.kt
@@ -2,14 +2,23 @@ package com.example.test.presentation
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.example.test.R
 import com.example.test.databinding.FragmentBookingBinding
 import com.example.test.viewmodel.BookingViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.tinkoff.decoro.MaskImpl
+import ru.tinkoff.decoro.slots.PredefinedSlots
+import ru.tinkoff.decoro.watchers.FormatWatcher
+import ru.tinkoff.decoro.watchers.MaskFormatWatcher
+
 
 class BookingFragment : Fragment() {
     private var _binding : FragmentBookingBinding? = null
@@ -28,8 +37,51 @@ class BookingFragment : Fragment() {
 
         _binding = FragmentBookingBinding.inflate(inflater, container, false)
         observeDataBooking()
+        maskPhone()
+
+        binding.etEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                if (s.toString().trim { it <= ' ' }.length == 0) {
+                } else {
+                    isValidEmail()
+                }
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                if (s.toString().trim { it <= ' ' }.length == 0) {
+                    isValidEmail()
+                }
+            }
+        })
+
 
         return binding.root
+    }
+
+    private fun isValidEmail() {
+        val emailPattern = Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")
+        val email: String = binding.etEmail.text.toString()
+        if (email.matches(emailPattern)) {
+            binding.etEmail.background =
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.bg_edit_text)
+        } else {
+            binding.etEmail.background =
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.bg_edittext_error)
+        }
+    }
+
+
+    private fun maskPhone() {
+        val mask = MaskImpl.createTerminated(PredefinedSlots.RUS_PHONE_NUMBER)
+        mask.isForbidInputWhenFilled = true
+        mask.setPlaceholder('*')
+        mask.setShowingEmptySlots(true)
+        val formatWatcher: FormatWatcher = MaskFormatWatcher(mask)
+        formatWatcher.installOn(binding.etPhone)
     }
 
     @SuppressLint("SetTextI18n")
